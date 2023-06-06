c962ab16d8acb9420a984ff350186204f02c4890
Dmitry Orlov <me@mosquito.su>
2023-06-06 22:07:57 +0300

    fix example

--- examples/memory/multiplication-decorator.py
+++ examples/memory/multiplication-decorator.py
@@ -1,7 +1,7 @@
 import asyncio
 from functools import reduce
 
-from patio import MemoryBroker, Registry
+from patio import MemoryBroker, Registry, AsyncExecutor
 
 
 rpc = Registry(project="test", strict=True)
@@ -14,12 +14,15 @@ async def mul(*args):
 
 
 async def main():
-    async with MemoryBroker(max_workers=4) as broker:
-        await broker.setup(registry=rpc)
+    async with AsyncExecutor(max_workers=50) as executor:
+        async with MemoryBroker(executor) as broker:
+            await broker.setup(registry=rpc)
 
-        print(
-            await asyncio.gather(*[broker.call(mul, i, i) for i in range(10)]),
-        )
+            print(
+                await asyncio.gather(*[
+                    broker.call(mul, i, i) for i in range(10)
+                ]),
+            )
 
 
 if __name__ == "__main__":
